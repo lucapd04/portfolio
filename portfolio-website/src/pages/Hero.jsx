1b41d141825e940d1235334319428afaccd24a56
Luca Porto Duarte <lduarte@uwo.ca>
2025-01-22 12:03:35 -0500

    updated hero banner

--- portfolio-website/src/pages/Hero.jsx
+++ portfolio-website/src/pages/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
     <>
       <Navbar/>
       <div className="flex flex-col items-start justify-center h-screen relative">
-        <Spline className="absolute inset-0 z-0" scene="https://prod.spline.design/EI8UlXehr28y9JMO/scene.splinecode" />
+        <Spline className="absolute inset-0 z-0" scene="https://prod.spline.design/EI8UlXehr28y9JMO/scene.splinecode" /> {/*banner changed*/}
         <div className="z-10 px-5 text-center md:ml-[5%] text-white md:text-left -mt-[12%] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           <h1 className="rubik-headline font-bold text-7xl mb-8">Welcome to my journey</h1>
           <h2 className="rubik-subtitle text-2xl ml-2 mb-20">My name is Luca Duarte, and I am a Computer Science student</h2>
